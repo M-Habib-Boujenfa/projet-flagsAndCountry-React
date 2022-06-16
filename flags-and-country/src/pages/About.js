3d3edb2e07f048df0bded3b233d0c1bdfd8a61d4
M-Habib-Boujenfa <habib-boujenfa@hotmail.fr>
2022-06-16 15:54:22 +0200

    composant about (page)

---
+++ flags-and-country/src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum nisi
+        quos a illo, esse earum in quod placeat commodi accusamus laborum qui
+        accusantium ducimus quae libero asperiores impedit nulla minus
+        laboriosam reiciendis quis rerum ipsa aliquid. Inventore obcaecati
+        voluptas consectetur vel beatae quod deserunt expedita in voluptatibus
+        nihil doloribus necessitatibus, eos eum accusamus eligendi voluptatem!
+        Dolorum necessitatibus consequuntur culpa eius voluptas quae rem
+        doloribus, reiciendis porro eligendi sit dolores accusantium modi
+        temporibus quisquam quam? Nisi voluptas minus sapiente numquam nemo
+        corrupti maiores distinctio quisquam earum fugiat odio facilis ipsum
+        pariatur, voluptates ratione labore autem quae minima consectetur
+        nostrum excepturi cum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates sed
+        eaque illum molestias facilis iure assumenda saepe laboriosam veritatis
+        veniam. Ex, animi vitae dignissimos numquam debitis libero eum
+        exercitationem ducimus incidunt consectetur dicta maxime dolore
+        asperiores repellat iure minus odit dolores laborum blanditiis,
+        molestiae nisi. Nihil temporibus porro quae, laboriosam nesciunt
+        veritatis incidunt soluta nostrum sunt perferendis sit explicabo
+        molestiae repellendus assumenda, praesentium culpa? Tenetur, consequatur
+        sed consectetur officiis optio, deleniti fugiat iste quos corrupti
+        dolor, enim aut quam. Delectus facere repudiandae amet, ipsam quibusdam
+        voluptas. Illo facere ipsum quas aspernatur ratione tempora, mollitia
+        praesentium cum voluptas. Labore assumenda, accusamus, quidem rerum vero
+        est error natus eveniet aliquid, modi voluptatibus quis magni illum
+        commodi quaerat soluta voluptate. Minima suscipit iste voluptates qui
+        perspiciatis ipsa veritatis corporis nobis iusto. Aperiam eius dolorem
+        fugiat minus. Culpa libero recusandae numquam aperiam animi hic dolore
+        ad cumque repellendus obcaecati incidunt debitis, itaque tempora odio
+        ipsum natus minima temporibus dolorum eaque modi id saepe dicta. Ullam
+        iure consequuntur, iste eveniet corporis aliquam, rem sequi dicta
+        perspiciatis nostrum, temporibus ratione possimus. Ad est totam a nobis
+        dolorem dicta voluptatibus atque exercitationem veniam, ut eius nisi
+        voluptate reprehenderit vel ipsum illo iusto autem corrupti aliquid
+        deserunt impedit.
+      </p>
+    </div>
+  );
+};
+
+export default About;
